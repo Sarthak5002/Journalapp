80db45f50b0fd901b9bfe8b3dcd0ccd83c861119
unknown <sarthakdengale4017@gmail.com>
2025-08-14 23:41:11 +0530

    Initial commit

---
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,36 @@
+package net.engineeringdigest.journalApp.controller;
+
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll() {
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public String createEntry(@RequestBody JournalEntry myEntry) {
+        journalEntries.put(myEntry.getId(), myEntry);
+        return "Successfully created";
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getEntryById(@PathVariable long myId) {
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteEntryById(@PathVariable long myId) {
+        return journalEntries.remove(myId);
+    }
+}
